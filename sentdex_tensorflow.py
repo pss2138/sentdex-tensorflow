bcf01f56295f7aa37b8657024ef705d7c572da5f
pss2138 <pss2138@gmail.com>
2020-10-03 10:12:13 +0900

    digit numbers recongnition

---
+++ sentdex_tensorflow.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+result = tf.multiply(x1,x2)
+
+with tf.compat.v1.Session() as sess:
+    output = sess.run(result)
+    print(output)
